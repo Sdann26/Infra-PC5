e7371902ee660fffb29c402eee23d2f6ad4fb27b
Danilo <sdann_26@hotmail.com>
2021-12-20 05:12:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
-    """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+@app.route('/home')
+def home():  
+    return "Esta es mi pagina web"; 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='http://af2c1894962354504a9bede9aef1a66d-1177657013.us-east-2.elb.amazonaws.com/')
